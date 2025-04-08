b193382482e13535f6d750a20965f655a00c54fe
Azaken1248 <suvkrm@gmail.com>
2025-04-08 16:42:54 +0530

    Made Crypto Quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css';
-import Home from './components/Home';
-import QuizPage from './components/QuizPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Home from "./components/Home";
+import QuizPage from "./components/QuizPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
